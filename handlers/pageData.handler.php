31d1132d6ec1614af85ccd3458e8db72591004ac
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-13 10:02:02 +0800

    feat: add handler demo
    - set data

---
+++ handlers/pageData.handler.php
@@ -0,0 +1,32 @@
+<?php
+$navList = [
+    ["label" => "Home", "url" => "/"],
+    ["label" => "About Us", "url" => "/about"],
+    ["label" => "Contact Us", "url" => "/contact"],
+];
+
+$tableColumnNames = [
+    "Product Name",
+    "Color",
+    "Category",
+    "Price",
+];
+
+$productList = [
+    [
+        "productName" => "Apple MacBook Pro 17",
+        "details" => [
+            "color" => ["Silver", "Black"],
+            "category" => "Laptop",
+            "price" => "$2999",
+        ],
+    ],
+    [
+        "productName" => "Microsoft Surface Pro",
+        "details" => [
+            "color" => ["White", "Black"],
+            "category" => "Laptop",
+            "price" => "$1999",
+        ],
+    ],
+];
