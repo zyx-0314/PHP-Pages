dc9ba53da4d44bb3d01531e6ce546ec80c4c4a09
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-13 10:03:43 +0800

    feat: add template components demo
    - added various application of fragmentation in template

---
+++ components/templates/head.component.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title><?= htmlEscape($pageTitle ?? 'My Shop') ?></title>
+
+        <!-- Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+
+    <body>
