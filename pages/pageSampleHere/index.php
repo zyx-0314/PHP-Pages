fbb5514007b6b5e1d008062cd9e34ad93e0c4b97
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-13 10:02:57 +0800

    fix: correct format of pages
    - fix initial implementation of index page and associated assets

---
+++ pages/pageSampleHere/index.php
@@ -0,0 +1,17 @@
+<?php
+require BASE_PATH . '/vendor/autoload.php';
+
+require BASE_PATH . '/utils/htmlEscape.utils.php';
+
+require BASE_PATH . '/handlers/pageData.handler.php';
+
+$pageTitle = 'Product List';
+
+include BASE_PATH . '/components/templates/head.component.php';
+include BASE_PATH . '/components/templates/nav.component.php';
+include BASE_PATH . '/components/templates/productTableList.component.php';
+include BASE_PATH . '/components/templates/foot.component.php';
+
+include BASE_PATH . '/components/cards/note.component.php';
+
+displayCardForm($productList);
