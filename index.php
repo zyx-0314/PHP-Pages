70eb49bd00ab3d830e50b5339ac7b2f92481ec74
chillyboy07 <bonneviechilly@gmail.com>
2025-05-20 21:28:16 +0800

    refactor: removed whitespaces

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ require BASE_PATH . "/vendor/autoload.php";
 require_once BASE_PATH . "/components/templates/head.component.php";
 require_once BASE_PATH . "/components/templates/nav.component.php";
 require_once BASE_PATH . "/components/templates/landing.component.php";
-
+   
 ?>
