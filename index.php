88dd254735416156ace315d5e4d5e6d8a8b53a37
chillyboy07 <bonneviechilly@gmail.com>
2025-05-20 21:25:11 +0800

    feat: added nav and landing component in index.php

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 require BASE_PATH . "/vendor/autoload.php";
 
 require_once BASE_PATH . "/components/templates/head.component.php";
+require_once BASE_PATH . "/components/templates/nav.component.php";
+require_once BASE_PATH . "/components/templates/landing.component.php";
 
 ?>
