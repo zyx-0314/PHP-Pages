44723c650f8c2660e7620a7474e435d3df540d7c
chillyboy07 <bonneviechilly@gmail.com>
2025-05-20 20:49:12 +0800

    feat: added link for font style in head component php

--- components/templates/head.component.php
+++ components/templates/head.component.php
@@ -2,5 +2,6 @@
     <title>Chilly's Guitars</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/example.css">
 </head>
