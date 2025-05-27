6c0ee433492c48a43c38b9736a7533893838f413
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-27 09:51:52 +0800

    feat: add product and song lists with detailed attributes

---
+++ staticData/dummy/productList.staticData.php
@@ -0,0 +1,76 @@
+<?php
+$productList = [
+    [
+        "productName" => "Apple MacBook Pro 17",
+        "details" => [
+            "color" => ["Silver", "Black"],
+            "category" => "Laptop",
+            "price" => 2999,
+        ],
+    ],
+    [
+        "productName" => "Microsoft Surface Pro",
+        "details" => [
+            "color" => ["White", "Black"],
+            "category" => "Laptop",
+            "price" => 1999,
+        ],
+    ],
+    [
+        "productName" => "Dell XPS 13",
+        "details" => [
+            "color" => ["Silver", "White"],
+            "category" => "Laptop",
+            "price" => 1499,
+        ],
+    ],
+    [
+        "productName" => "HP Spectre x360",
+        "details" => [
+            "color" => ["Black", "Blue"],
+            "category" => "Laptop",
+            "price" => 1599,
+        ],
+    ],
+    [
+        "productName" => "Lenovo ThinkPad X1 Carbon",
+        "details" => [
+            "color" => ["Black"],
+            "category" => "Laptop",
+            "price" => 1799,
+        ],
+    ],
+    [
+        "productName" => "Asus ROG Zephyrus G14",
+        "details" => [
+            "color" => ["Gray", "White"],
+            "category" => "Gaming Laptop",
+            "price" => 2199,
+        ],
+    ],
+    [
+        "productName" => "Acer Swift 3",
+        "details" => [
+            "color" => ["Silver"],
+            "category" => "Laptop",
+            "price" => 999,
+        ],
+    ],
+    [
+        "productName" => "Google Pixelbook Go",
+        "details" => [
+            "color" => ["Black", "Pink"],
+            "category" => "Chromebook",
+            "price" => 849,
+        ],
+    ],
+    [
+        "productName" => "Lenovo Legion",
+        "details" => [
+            "color" => ["Black", "Pink"],
+            "category" => "Chromebook",
+            "price" => 849,
+        ],
+    ],
+];
+?>
---
+++ staticData/dummy/songList.staticData.php
@@ -0,0 +1,74 @@
+<?php
+
+$songPlaylist = [
+    [
+        "title" => "Shape of You",
+        "author" => "Ed Sheeran",
+        "genre" => ["Pop"],
+        "downloadDate" => "2025-05-01 14:23:00",
+        "price" => 121232.29,
+    ],
+    [
+        "title" => "Bohemian Rhapsody",
+        "author" => "Queen",
+        "genre" => ["Rock", "Classic Rock"],
+        "downloadDate" => "2025-05-02 09:15:00",
+        "price" => 112321.49,
+    ],
+    [
+        "title" => "Blinding Lights",
+        "author" => "The Weeknd",
+        "genre" => ["Synthpop", "R&B"],
+        "downloadDate" => "2025-05-03 18:45:00",
+        "price" => 1332.19,
+    ],
+    [
+        "title" => "Bad Guy",
+        "author" => "Billie Eilish",
+        "genre" => ["Pop", "Electropop"],
+        "downloadDate" => "2025-05-04 12:10:00",
+        "price" => 2342.09,
+    ],
+    [
+        "title" => "Uptown Funk",
+        "author" => "Mark Ronson ft. Bruno Mars",
+        "genre" => ["Funk", "Pop"],
+        "downloadDate" => "2025-05-05 16:30:00",
+        "price" => 34234.39,
+    ],
+    [
+        "title" => "Imagine",
+        "author" => "John Lennon",
+        "genre" => ["Soft Rock"],
+        "downloadDate" => "2025-05-06 08:00:00",
+        "price" => 123123131232.29,
+    ],
+    [
+        "title" => "Levitating",
+        "author" => "Dua Lipa",
+        "genre" => ["Pop", "Disco"],
+        "downloadDate" => "2025-05-07 20:20:00",
+        "price" => 431.19,
+    ],
+    [
+        "title" => "Hotel California",
+        "author" => "Eagles",
+        "genre" => ["Rock", "Classic Rock"],
+        "downloadDate" => "2025-05-08 11:55:00",
+        "price" => 12312312.59,
+    ],
+    [
+        "title" => "Thinking Out Loud",
+        "author" => "Ed Sheeran",
+        "genre" => ["Pop", "Soul"],
+        "downloadDate" => "2025-05-09 13:40:00",
+        "price" => 1232.29,
+    ],
+    [
+        "title" => "Can't Stop the Feeling!",
+        "author" => "Justin Timberlake",
+        "genre" => ["Pop", "Dance"],
+        "downloadDate" => "2025-05-10 17:05:00",
+        "price" => 1231.09,
+    ],
+];
