33942bf3b088b9c38b234de71187fe6748d2ce44
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-27 08:21:39 +0800

    feat: implement main layout rendering function

---
+++ layout/main.layout.php
@@ -0,0 +1,24 @@
+<?php
+function renderMainLayout(callable $content)
+{
+    // Libraries
+    require BASE_PATH . '/vendor/autoload.php';
+    require BASE_PATH . '/utils/htmlEscape.utils.php';
+
+    // Templates
+    require_once BASE_PATH . '/components/templates/foot.component.php';
+    require_once BASE_PATH . '/components/templates/head.component.php';
+    require_once BASE_PATH . '/components/templates/nav.component.php';
+
+    // Datas
+    require_once BASE_PATH . "/staticData/navPages.php";
+
+    // Code Body
+    head("Product List");
+    navHeader($headNavList);
+
+    // This is the content
+    $content();
+
+    footer();
+}
