6e1702ef36613af487cca2ea383d5dab2aeccec3
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-27 08:21:52 +0800

    feat: add new product and song entries to data handler

--- handlers/pageData.handler.php
+++ handlers/pageData.handler.php
@@ -1,10 +1,4 @@
 <?php
-$navList = [
-    ["label" => "Home", "url" => "/"],
-    ["label" => "News", "url" => "/contact"],
-    ["label" => "About Us", "url" => "/about"],
-    ["label" => "Contact Us", "url" => "/contact"],
-];
 
 $tableColumnNames = [
     "Product Name",
@@ -78,6 +72,14 @@ $productList = [
             "price" => 849,
         ],
     ],
+    [
+        "productName" => "Lenovo Legion",
+        "details" => [
+            "color" => ["Black", "Pink"],
+            "category" => "Chromebook",
+            "price" => 849,
+        ],
+    ],
 ];
 
 $accounts = array(
@@ -86,3 +88,84 @@ $accounts = array(
     "https://gmail.com",
 
 );
+
+$itinerary = [
+    "Day 1: Lorem 1",
+    "Day 2: Lorem 2",
+    "Day 3: Lorem 3",
+];
+
+$songPlaylist = [
+    [
+        "title" => "Shape of You",
+        "author" => "Ed Sheeran",
+        "genre" => ["Pop"],
+        "downloadDate" => "2025-05-01 14:23:00",
+        "price" => 121232.29,
+    ],
+    [
+        "title" => "Bohemian Rhapsody",
+        "author" => "Queen",
+        "genre" => ["Rock", "Classic Rock"],
+        "downloadDate" => "2025-05-02 09:15:00",
+        "price" => 112321.49,
+    ],
+    [
+        "title" => "Blinding Lights",
+        "author" => "The Weeknd",
+        "genre" => ["Synthpop", "R&B"],
+        "downloadDate" => "2025-05-03 18:45:00",
+        "price" => 1332.19,
+    ],
+    [
+        "title" => "Bad Guy",
+        "author" => "Billie Eilish",
+        "genre" => ["Pop", "Electropop"],
+        "downloadDate" => "2025-05-04 12:10:00",
+        "price" => 2342.09,
+    ],
+    [
+        "title" => "Uptown Funk",
+        "author" => "Mark Ronson ft. Bruno Mars",
+        "genre" => ["Funk", "Pop"],
+        "downloadDate" => "2025-05-05 16:30:00",
+        "price" => 34234.39,
+    ],
+    [
+        "title" => "Imagine",
+        "author" => "John Lennon",
+        "genre" => ["Soft Rock"],
+        "downloadDate" => "2025-05-06 08:00:00",
+        "price" => 123123131232.29,
+    ],
+    [
+        "title" => "Levitating",
+        "author" => "Dua Lipa",
+        "genre" => ["Pop", "Disco"],
+        "downloadDate" => "2025-05-07 20:20:00",
+        "price" => 431.19,
+    ],
+    [
+        "title" => "Hotel California",
+        "author" => "Eagles",
+        "genre" => ["Rock", "Classic Rock"],
+        "downloadDate" => "2025-05-08 11:55:00",
+        "price" => 12312312.59,
+    ],
+    [
+        "title" => "Thinking Out Loud",
+        "author" => "Ed Sheeran",
+        "genre" => ["Pop", "Soul"],
+        "downloadDate" => "2025-05-09 13:40:00",
+        "price" => 1232.29,
+    ],
+    [
+        "title" => "Can't Stop the Feeling!",
+        "author" => "Justin Timberlake",
+        "genre" => ["Pop", "Dance"],
+        "downloadDate" => "2025-05-10 17:05:00",
+        "price" => 1231.09,
+    ],
+];
+
+$carDescription = "Audi, 2020 Model, V8 Engine, Metalic Gray, has Decals, 8Liter";
