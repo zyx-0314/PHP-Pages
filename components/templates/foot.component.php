6b02035a60b18e43c87fcccf263bde49d17a14b5
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-27 13:32:50 +0800

    refactor: update template components

--- components/templates/foot.component.php
+++ components/templates/foot.component.php
@@ -1,7 +1,13 @@
 <?php
+
+include_once BASE_PATH . "/components/templates/footer.component.php";
+
 function footer()
 {
+    footerComponent();
     ?>
+
+
     </body>
 
     </html>
--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -3,21 +3,23 @@ function navHeader($navList)
 {
     ?>
     <header>
-        <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav class="bg-gray-300 dark:bg-gray-500 px-4 lg:px-6 py-2.5 border-gray-200">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                <a href="https://flowbite.com" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Nyebe Sample</span>
+                <a href="/" class="flex items-center">
+                    <img src="./assets/img/nyebe_white.png" class="mr-3 h-6 sm:h-9" alt="Nyebe Logo" />
+                    <span class="self-center font-semibold dark:text-white text-lg whitespace-nowrap">
+                        <span class="font-black">
+                            NYEBE:
+                        </span>
+                        AD CI4 Template
+                        Guide</span>
                 </a>
                 <div class="flex items-center lg:order-2">
                     <a href="#"
-                        class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log
-                        in</a>
-                    <a href="#"
-                        class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get
+                        class="bg-primary-700 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 mr-2 px-4 lg:px-5 py-2 lg:py-2.5 rounded-lg focus:outline-none focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-800 font-medium text-white text-sm">Get
                         started</a>
                     <button data-collapse-toggle="mobile-menu-2" type="button"
-                        class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        class="lg:hidden inline-flex items-center hover:bg-gray-100 dark:hover:bg-gray-700 ml-1 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-600 text-gray-500 dark:text-gray-400 text-sm"
                         aria-controls="mobile-menu-2" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -33,14 +35,14 @@ function navHeader($navList)
                         </svg>
                     </button>
                 </div>
-                <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                    <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <div class="hidden lg:flex justify-between items-center lg:order-1 w-full lg:w-auto" id="mobile-menu-2">
+                    <ul class="flex lg:flex-row flex-col lg:space-x-8 mt-4 lg:mt-0 font-medium">
                         <?php
                         foreach ($navList as $nav):
                             ?>
                             <li>
-                                <a href="#"
-                                    class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                                <a href="<?php echo $nav["link"] ?>"
+                                    class="block bg-primary-700 lg:bg-transparent lg:p-0 py-2 pr-4 pl-3 rounded text-white lg:text-primary-700 dark:text-white"
                                     aria-current="page"><?php echo $nav["label"] ?></a>
                             </li>
                             <?php
