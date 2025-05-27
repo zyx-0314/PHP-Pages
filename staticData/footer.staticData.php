495b0789e2e9c22abc8b94a076814c9a9f732c86
zyx-0314-FJorn <ramirezian037@gmail.com>
2025-05-27 13:33:25 +0800

    feat: added static datas

---
+++ staticData/footer.staticData.php
@@ -0,0 +1,34 @@
+<?php
+$footerLinks = [
+    [
+        "title" => "Docs",
+        'subs' => [
+            [
+                'tag' => 'MongoDB',
+                'link' => '/mongodb',
+            ],
+            [
+                'tag' => 'PostgreSQL',
+                'link' => '/postgresql',
+            ],
+            [
+                'tag' => 'Postman',
+                'link' => '/postman',
+            ],
+        ]
+    ],
+    [
+        'title' => 'Resources',
+        'subs' => [
+            [
+                'tag' => 'VS Code Profile',
+                'link' => './assets/vsCode/PHP-CI4-AITS.code-profile',
+                'download' => 'PHP-CI4-AITS.code-profile',
+            ],
+            [
+                'tag' => 'PHP Template',
+                'link' => 'https://github.com/zyx-0314/AD-CI4-Template-Project',
+            ],
+        ]
+    ],
+];
--- staticData/navPages.staticData.php
+++ staticData/navPages.staticData.php
@@ -1,7 +1,7 @@
 <?php
 $headNavList = [
-    ["label" => "Home", "url" => "/"],
-    ["label" => "MongoDB", "url" => "/mongodb"],
-    ["label" => "PostgreSql", "url" => "/postgresql"],
-    ["label" => "Postman", "url" => "/contact"],
+    ["label" => "Home", "link" => "/"],
+    ["label" => "MongoDB", "link" => "/pages/mongodb/index.php"],
+    ["label" => "PostgreSql", "link" => "/pages/postgresql/index.php"],
+    ["label" => "Postman", "link" => "/pages/postman/index.php"],
 ];
